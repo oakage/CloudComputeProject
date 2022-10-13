ba7a3aae2255d731fbcf0498288a74ef247f8d79
Lopez <c-llopez1@allegion.com>
2022-10-13 12:45:05 -0700

    cambio de proy

---
+++ CloudCompute/Server/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CloudCompute.Shared;
+
+namespace CloudCompute.Server
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<CDs> cds { get; set; }
+    }
+}
---
+++ CloudCompute/Shared/CDs.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CloudCompute.Shared
+{
+    public class CDs
+    {
+        public CDs()
+        {
+            idCD = 0;
+            CDName = "";
+            Artist = "";
+            Genre = "";
+            Arrive = DateTime.Now;
+        }
+
+        public CDs(int? idCD, string? CDName, string? Artist, string? Genre, int? Quantity, DateTime? Arrive)
+        {
+            this.idCD = idCD;
+            this.CDName = CDName;
+            this.Artist = Artist;
+            this.Genre = Genre;
+            this.Quantity = Quantity;
+            this.Arrive = Arrive;
+        }
+
+        public int? idCD { get; set; }
+        [Required(ErrorMessage = "CD Name is required")]
+        public string? CDName { get; set; }
+        [Required(ErrorMessage = "Artist name is required")]
+        public string? Artist { get; set; }
+        [Required(ErrorMessage = "Genre field is required")]
+        public string? Genre { get; set; }
+        public int? Quantity { get; set; }
+        [Required(ErrorMessage = "Arrival date is required")]
+        public DateTime? Arrive { get; set; }
+    }
+}
